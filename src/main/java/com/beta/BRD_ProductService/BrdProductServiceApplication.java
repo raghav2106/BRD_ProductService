b6e220ab07c7a43871ed7a94a7e1ebc55413fe5c
raghav2106 <kraghava112@gmail.com>
2024-05-20 08:54:39 +0530

    initial commit

---
+++ src/main/java/com/beta/BRD_ProductService/BrdProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.beta.BRD_ProductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrdProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrdProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beta/BRD_ProductService/BrdProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beta.BRD_ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrdProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
